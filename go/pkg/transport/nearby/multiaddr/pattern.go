642215bb60320b7bc1072d92ed8bdd5ce85d385e
aeddi <antoine.e.b@gmail.com>
2020-03-10 23:25:25 +0100

    test: ci

--- go/pkg/transport/nearby/multiaddr/pattern.go
+++
@@ -1,9 +0,0 @@
-package multiaddr
-
-import (
-	mafmt "github.com/multiformats/go-multiaddr-fmt"
-)
-
-// Nearby multiaddr validation checker
-// See https://github.com/multiformats/go-multiaddr-fmt
-var NEARBY = mafmt.Base(P_NEARBY)
--- go/pkg/transport/nearby/native.go
+++
@@ -1,7 +0,0 @@
-package nearby
-
-type NativeDriver interface {
-	Start(localPeerID string) error
-	Stop() error
-	Dial(remotePeerID string) bool
-}
